3f304c4f98a6265ed7657cb45d2792abe5eababb
TheShadowGhost745 <106347640+TheShadowGhost745@users.noreply.github.com>
2022-05-27 21:41:23 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -5,9 +5,10 @@ import math
 class AnimateSprite(pygame.sprite.Sprite):
 
     # definir les choses à faire à la création de l'entité
-    def __init__(self, sprite_name):
+    def __init__(self, sprite_name, image_number):
         super().__init__()
         self.image = pygame.image.load(f"assets/Images/sprites/{sprite_name}.png")
+        self.image_number = image_number
         self.current_image = 0 # commencer l'anim à l'image 0
         self.images = animations.get(sprite_name)
         self.animation = False
@@ -16,7 +17,7 @@ class AnimateSprite(pygame.sprite.Sprite):
         self.animation = True
 
     # definir une methode pour animer le sprite
-    def animate(self):
+    def animate(self, loop=False, flip=False):
 
         # vérifier si l'animation est active
         if self.animation:
@@ -29,8 +30,16 @@ class AnimateSprite(pygame.sprite.Sprite):
                 # remettre l'animation au départ
                 self.current_image = 0
 
-            # modifier l'image précedente par la suivante
-            self.image = self.images[math.floor(self.current_image)]
+                if not loop:
+                    # désactiver l'animation
+                    self.animation = False
+
+            if not flip:
+                # modifier l'image précedente par la suivante
+                self.image = self.images[math.floor(self.current_image)]
+                return
+
+            self.image = pygame.transform.flip(self.images[math.floor(self.current_image)], True, False)
 
 # definir une fonction pour charger les images d'un sprite
 def load_animation_images(sprite_name):
@@ -39,9 +48,13 @@ def load_animation_images(sprite_name):
     path = f"assets/Images/sprites/{sprite_name}/{sprite_name}"
 
     # boucler sur chaque image dans ce dossier
-    for num in range(1, 3):
-        image_path = path + str(num) + ".png"
-        images.append(pygame.image.load(image_path))
+    for num in range(1, 100):
+        try:
+            image_path = path + str(num) + ".png"
+            images.append(pygame.image.load(image_path))
+
+        except FileNotFoundError:
+            break
 
     # renvoyer le contenu de la liste d'image
     return images
@@ -51,5 +64,7 @@ def load_animation_images(sprite_name):
 animations = {
     "YellowKoopa": load_animation_images("YellowKoopa"),
     "BlueKoopa": load_animation_images("BlueKoopa"),
-    "RedKoopa": load_animation_images("RedKoopa")
+    "RedKoopa": load_animation_images("RedKoopa"),
+    "mario2": load_animation_images("mario2"),
+    "luigi": load_animation_images("luigi")
 }
